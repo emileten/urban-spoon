7049fed93b069c3acdb16bc9954d6aa66551700c
Emile Tenezakis <e.tenezakis@gmail.com>
2021-12-14 14:03:21 +0900

    moved write dataset

--- urbanspoon/tests/test_services.py
+++ urbanspoon/tests/test_services.py
@@ -8,7 +8,7 @@ from urbanspoon.tests import conftest
 def test_map_periods_average():
     fakedata = xr.Dataset({'fakevariable':conftest.spatio_temporal_gcm_factory(np.random.rand(4, 361, 721))})
     fakedata_storage = "memory://fakedata_map_periods_average.zarr"
-    conftest.write_dataset(ds=fakedata, url_or_path=fakedata_storage)
+    repository.write_dataset(ds=fakedata, url_or_path=fakedata_storage)
     with tempfile.NamedTemporaryFile() as outfile:
         services.map_period_average(inputfile=fakedata_storage, var="fakevariable", color_bar_range=(0, 1), outputfile=f"{outfile.name}.png", periods=[("1995-01-01","1995-01-02"),("1995-01-03","1995-01-04")])
         assert os.path.isfile(f"{outfile.name}.png")
